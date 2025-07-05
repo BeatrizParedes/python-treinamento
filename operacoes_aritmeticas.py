bf98ad7ac91f101337558754875e4e95ea0b5fcb
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-07-04 23:41:50 -0300

    Operações aritméticas

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,16 @@
+"Desenvolva um programa que declare duas variáveis inteiras e realize as operações de:"
+"Soma, subtração, multiplicação, divisão e módulo entre elas"
+"Exiba os resultados de cada operação"
+
+def calculadora():
+    num1 = float (input("Insira o primeiro número "))
+    num2 = float (input("Insira o segundo número "))
+    
+    print(f"{num1} - {num2} = ", num1-num2)
+    print(f"{num1} + {num2} = ", num1+num2)
+    print(f"{num1} * {num2} = ", num1*num2)
+    print(f"{num1} / {num2} = ", num1/num2)
+    print(f"{num1} % {num2} = ", num1%num2) #Operação de módulo
+
+if __name__ == "__main__":
+    calculadora()
