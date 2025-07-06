128a2ede168851c278fabf5f8e4c1c5a35254c96
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-07-06 11:52:29 -0300

    Exercício maior número inteiro

---
+++ maior_de_tres.py
@@ -0,0 +1,16 @@
+"Desenvolva um programa que leia três números inteiros e exiba o maior deles"
+
+def maior():
+    num1=int(input("Escolha o primeiro número: "))
+    num2=int(input("Escolha o segundo número: "))
+    num3=int(input("Escolha o terceiro número: "))
+
+    if (num1>num2&num1>num3):
+        print(f"O {num1} é o maior número entre os que você escolheu")
+    elif (num2>num1&num2>num3):
+        print(f"O {num2} é o maior número entre os que você escolheu")
+    else:
+        print(f"O {num3} é o maior dos números que você escolheu")
+
+if __name__=="__main__":
+    maior()
