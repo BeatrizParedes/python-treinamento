c4f3a6383dd3f507da34336b20bfbd2febcbd8a3
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-07-06 18:40:18 -0300

    Incremento e decremento

---
+++ incremento_e_decremento.py
@@ -0,0 +1,17 @@
+"Escreva um programa que declare uma variável inteira"
+"Aplique o operador de incremento (usando +=1)"
+"E decremento (usando -=1)"
+"E exiba o valor da variável antes e depois de cada operação"
+
+def incremento_decremento():
+    variavel=10
+    print(variavel)
+
+    variavel+=1
+    print(variavel)
+
+    variavel-=1
+    print(variavel)
+
+if __name__=="__main__":
+    incremento_decremento()
