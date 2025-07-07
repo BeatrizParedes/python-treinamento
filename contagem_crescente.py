77140513caf6a6d99f5dfe2ccdb63cc0fd1b2a17
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-07-07 18:14:46 -0300

    Exercício de ordenamento usando o for

---
+++ contagem_crescente.py
@@ -0,0 +1,7 @@
+"Escreva um programa que exiba os números de 1 a 10, em ordem crescente, usando m laço for"
+
+def ordem():
+    for i in range (1,11): #O valor final 11 não é incluído
+        print(i)
+if __name__=="__main__":
+    ordem()
