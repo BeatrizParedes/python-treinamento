68912bfca556631bfa7a1eac50c47fa3227ff327
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-08-18 16:41:04 -0300

    Exercício maior e menor número inteiro de um array

---
+++ menor_e_maior_valor.py
@@ -0,0 +1,24 @@
+"Desenvolva um programa que leia 10 números inteiros e armazene-os em um array."
+"Encontre e exiba o menor e o maior valor presentes no array"
+
+def menor_e_maior():
+    numeros=[]
+    maior=menor=numeros[0]
+
+    for i in range(10):
+        numero=int(input(f"Digite o numero {i+1} "))
+        numeros.append(numero)
+    
+    
+
+    for numero in numeros:
+        if numero>maior:
+            maior=numero
+        if numero<menor:
+            menor=numero
+    
+    print(f"O maior número é o {maior}")
+    print(f"O menor número é o {menor}")
+
+if __name__== "__main__":
+    menor_e_maior()
